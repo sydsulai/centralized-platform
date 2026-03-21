13864c607fa00906247bf558d8b4df5740681cc2
sydsulai <sydport11@gmail.com>
2026-03-21 11:59:44 +0530

    Istio Config in Application Cluster

--- application-infrastructure/eks.tf
+++ application-infrastructure/eks.tf
@@ -52,17 +52,6 @@ resource "aws_eks_addon" "aws_secrets_store_csi_driver_provider" {
     ]
 }
 
-resource "kubernetes_service_account_v1" "ums_pod_identity_deployment_sa" {
-    metadata {
-        name      = "ums-pod-identity-deployment-sa"
-        namespace = "ums-ns"
-    }
-
-    depends_on = [
-        aws_eks_cluster.app_cluster_01
-    ]
-}
-
 resource "aws_eks_pod_identity_association" "ums_pod_identity_deployment_sa" {
     cluster_name    = aws_eks_cluster.app_cluster_01.name
     namespace       = "ums-ns"
@@ -90,37 +79,8 @@ resource "aws_eks_fargate_profile" "app_cluster_01_fargate_profile" {
 
     depends_on = [
         aws_iam_role_policy_attachment.eks_fargate_pod_execution_policy,
-        aws_eks_cluster.app_cluster_01
-    ]
-}
-
-resource "kubernetes_service_account_v1" "aws_load_balancer_controller" {
-    metadata {
-        name      = "aws-load-balancer-controller"
-        namespace = "kube-system"
-        annotations = {
-            "eks.amazonaws.com/role-arn" = aws_iam_role.aws_load_balancer_controller_irsa.arn
-        }
-    }
-
-    depends_on = [
         aws_eks_cluster.app_cluster_01,
-        aws_iam_role_policy_attachment.aws_load_balancer_controller_irsa_policy_attachment
-    ]
-}
-
-resource "kubernetes_service_account_v1" "xray_daemon" {
-    metadata {
-        name      = "xray-daemon"
-        namespace = "default"
-        annotations = {
-            "eks.amazonaws.com/role-arn" = aws_iam_role.xray_daemon_irsa.arn
-        }
-    }
-
-    depends_on = [
-        aws_eks_cluster.app_cluster_01,
-        aws_iam_role_policy_attachment.xray_daemon_irsa_policy_attachment
+        kubernetes_namespace_v1.app2_fargate_ns
     ]
 }
 
@@ -136,8 +96,8 @@ resource "aws_eks_node_group" "app_cluster_01_ng_private1" {
     disk_size      = 20
 
     scaling_config {
-        desired_size = 2
-        min_size     = 2
+        desired_size = 3
+        min_size     = 3
         max_size     = 4
     }
 
@@ -157,35 +117,35 @@ resource "aws_eks_node_group" "app_cluster_01_ng_private1" {
     ]
 }
 
-resource "aws_eks_node_group" "app_cluster_01_ng_public1" {
-    cluster_name    = aws_eks_cluster.app_cluster_01.name
-    node_group_name = "app-cluster-01-ng-public1"
-    node_role_arn   = aws_iam_role.eks_nodegroup.arn
-    subnet_ids = [
-        aws_subnet.app_vpc_public_subnets.id, # public
-        aws_subnet.app_vpc_public_subnets_2.id, # public
-    ]
-    instance_types = ["t3.2xlarge"]
-    disk_size      = 20
+# resource "aws_eks_node_group" "app_cluster_01_ng_public1" {
+#     cluster_name    = aws_eks_cluster.app_cluster_01.name
+#     node_group_name = "app-cluster-01-ng-public1"
+#     node_role_arn   = aws_iam_role.eks_nodegroup.arn
+#     subnet_ids = [
+#         aws_subnet.app_vpc_public_subnets.id, # public
+#         aws_subnet.app_vpc_public_subnets_2.id, # public
+#     ]
+#     instance_types = ["t3.2xlarge"]
+#     disk_size      = 20
 
-    scaling_config {
-        desired_size = 2
-        min_size     = 2
-        max_size     = 4
-    }
+#     scaling_config {
+#         desired_size = 2
+#         min_size     = 2
+#         max_size     = 4
+#     }
 
-    remote_access {
-        ec2_ssh_key = "my-vpc-01-keypair"
-    }
+#     remote_access {
+#         ec2_ssh_key = "my-vpc-01-keypair"
+#     }
 
-    depends_on = [
-        aws_iam_role_policy_attachment.eks_nodegroup_worker_node_policy,
-        aws_iam_role_policy_attachment.eks_nodegroup_cni_policy,
-        aws_iam_role_policy_attachment.eks_nodegroup_ecr_readonly,
-        aws_iam_role_policy_attachment.eks_nodegroup_asg_access,
-        aws_iam_role_policy_attachment.eks_nodegroup_external_dns_access,
-        aws_iam_role_policy_attachment.eks_nodegroup_full_ecr_access,
-        # aws_iam_role_policy_attachment.eks_nodegroup_appmesh_access,
-        aws_iam_role_policy_attachment.eks_nodegroup_alb_ingress_access
-    ]
-}
+#     depends_on = [
+#         aws_iam_role_policy_attachment.eks_nodegroup_worker_node_policy,
+#         aws_iam_role_policy_attachment.eks_nodegroup_cni_policy,
+#         aws_iam_role_policy_attachment.eks_nodegroup_ecr_readonly,
+#         aws_iam_role_policy_attachment.eks_nodegroup_asg_access,
+#         aws_iam_role_policy_attachment.eks_nodegroup_external_dns_access,
+#         aws_iam_role_policy_attachment.eks_nodegroup_full_ecr_access,
+#         # aws_iam_role_policy_attachment.eks_nodegroup_appmesh_access,
+#         aws_iam_role_policy_attachment.eks_nodegroup_alb_ingress_access
+#     ]
+# }
