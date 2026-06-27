1f64f08803cda8de09b7f84df4f1dbc60c0bb2c0
sydsulai <sydport11@gmail.com>
2026-06-27 11:50:46 +0530

    RAW Manifests for centralized cluster

--- centralized-cluster/cluster-configuration/addons_manifests.sh
+++ centralized-cluster/cluster-configuration/addons_manifests.sh
@@ -8,7 +8,7 @@ helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
   --set serviceAccount.create=false \
   --set serviceAccount.name=aws-load-balancer-controller \
   --set region=ap-south-1 \
-  --set vpcId=vpc-0b8c041e5cb2a3f3d \
+  --set vpcId=vpc-0f4aee78ed43774ab \
   --set image.repository=public.ecr.aws/eks/aws-load-balancer-controller
 
 # Istio
@@ -20,10 +20,16 @@ kubectl label namespace default istio-injection=enabled --overwrite
 
 
 export PLATFORM_CLUSTER=$(kubectl config get-contexts -o name | grep -i platform-cluster)
+
+# ISTIOCTL configuration setup
 istioctl install --context="${PLATFORM_CLUSTER}" -f istio-config.yaml
 istioctl --context="${PLATFORM_CLUSTER}" install -y -f eastwest-gateway.yaml
 kubectl --context="${PLATFORM_CLUSTER}" apply -n istio-system -f expose-services.yaml
-
-
 istioctl --context="${PLATFORM_CLUSTER}" uninstall -y -f istio-config.yaml
 istioctl --context="${PLATFORM_CLUSTER}" uninstall -y -f eastwest-gateway.yaml
+
+# RAW manifests for Istio Control Plane and save it to a file
+istioctl manifest generate -f istio-config.yaml >> istio-installation-manifests.yaml
+
+# Delete all resources in a namespace
+kubectl delete $(kubectl api-resources --namespaced=true --verbs=delete -o name | tr "\n" "," | sed 's/,$//') --all -n istio-system
